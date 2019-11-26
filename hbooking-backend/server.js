d1aa3a41936b22fff8de75ed97b3c5bbb6c45d05
nguyensonhai <nguyensonhai1009@gmail.com>
2019-11-26 11:43:03 +0700

    update

--- hbooking-backend/server.js
+++ hbooking-backend/server.js
@@ -12,7 +12,7 @@ const customerRouter = require("./customers/customers.routes");
 const bookingRouter = require("./booking/booking.routes");
 
 mongoose.connect(
-  "mongodb+srv://admin:04030211@admin@hbooking-nxjjx.mongodb.net/test?retryWrites=true&w=majority",
+  "mongodb://localhost:27017/hbooking",
   { useNewUrlParser: true, useUnifiedTopology: true },
   e => {
     if (e) {
@@ -25,7 +25,7 @@ mongoose.connect(
       app.use(express.static("public"));
       app.use(
         cors({
-          origin: ["ezhbooking.herokuapp.com"],
+          origin: ["http://localhost:3000"],
           credentials: true
         })
       );
